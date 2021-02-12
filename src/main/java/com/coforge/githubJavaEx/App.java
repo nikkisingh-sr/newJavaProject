3c1b4f4c166699cb2f45ee7761e1af798d48e68a
NIKKI SINGH <NIKKI SINGH@DESKTOP-PA1B6P0>
2021-02-12 13:28:39 +0530

    New commit

--- src/main/java/com/coforge/githubJavaEx/App.java
+++ src/main/java/com/coforge/githubJavaEx/App.java
@@ -1,5 +1,9 @@
 package com.coforge.githubJavaEx;
 
+import java.util.Scanner;
+
+import com.coforge.githubJavaEx.GithubJavaProject.service.EmployeeServiceImpl;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        EmployeeServiceImpl e=new EmployeeServiceImpl();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter 1for addemp and 2 2 for Display");
+        switch(sc.nextInt()) {
+        case 1:e.addEmp();
+        case 2:e.displayEmp();
+             break;
+        default: System.out.println("Requesting");
+        }
     }
 }
